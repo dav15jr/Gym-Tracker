55198b5c6657685fcd346665c7c410675994cf0b
dav15jr <dnwanderajr@outlook.com>
2024-02-13 20:03:12 +0100

    Add local storage functionality, and display

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,21 @@ function App() {
       <h1>Gym Tracker</h1>
       <div className="card">
         <form className="form">
-          Exercise:
-          <input type="text" placeholder='Enter Exercise'></input>
-          <select>
+          <label for="exercise">Exercise:</label>
+          <input type="text" placeholder='Enter Exercise' name="exercise" id="exercise"></input>
+          <label for="ex-type">Exercise Type:</label>
+          <select name="ex-type">
             <option value="bw">Body Weight</option>
             <option value="resistance">Resistance</option>
             <option value="kg">Kilo Grams (Kg's)</option>
             <option value="lb">Pounds (Lb's)</option>
           </select>
-          <input type="number" placeholder='Amount'></input>
-          <input type="number" placeholder='Reps'></input>
-          <input type="number" placeholder='Sets'></input>
+          <label for="amount">Amount:</label>
+          <input type="number" placeholder='Amount' name="amount"></input>
+          <label for="reps">No. of Reps:</label>
+          <input type="number" placeholder='Reps' name="reps"></input>
+          <label for="sets">No. of Sets:</label>
+          <input type="number" placeholder='Sets' name="sets"></input>
           <button id="submit-btn" type="submit" >Save Exercise</button>
         </form>
         <button onClick={() => setCount((count) => count + 1)}>
