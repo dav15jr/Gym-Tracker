2d2c8b9f31218827939cbab8a953d60a2140ad92
dav15jr <dnwanderajr@outlook.com>
2024-02-15 21:42:28 +0100

    Added button to finish individual exercise's and change text and button styling to show state of the exercise. Able to toggle the state as well incase of mistake.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
 // import { useState } from 'react'
-import './App.css'
+import './index.css'
 
 export default function App() {
 
     window.onload=function(){     /*---Runs once the app is loaded---*/
      
-    
 //-------------------Save Exercise inputs --------------------------------    
     
     const form = document.querySelector('form');
-    
-    
-      form.addEventListener('submit', (e) =>  {
+        
+    form.addEventListener('submit', (e) =>  {
         e.preventDefault();
         
-
         // let oldItems = JSON.parse(localStorage.getItem("form"));
         // if(oldItems == null) oldItems = [];
 
         const fd = new FormData(form);
         const obj = Object.fromEntries(fd);
 
-        const keyname = obj.exercise;   // Take the exercise name and save it to be used as the key name for local storage.
+        const keyname = obj.Exercise;   // Take the exercise name and save it to be used as the key name for local storage.
 
         const json = JSON.stringify(obj);
         localStorage.setItem(`${keyname}`, json);
@@ -33,76 +30,82 @@ export default function App() {
 
 //----------------------- Display Exercises ----------------
 
-           for (const key in data) {
-                            
-                  const markup= `            
-                  <div>
-                    <span>${key}: ${data[key]}</span>
-                  </div>
-                  `;
-                  document.getElementById('data').innerHTML += markup;
+        const newDiv = document.createElement("div")
+        const newBtn = document.createElement("button")
+        newDiv.className = `${keyname}`;
+        newDiv.id = `${keyname}`;
+        newBtn.id = `${keyname}btn`;
+        newBtn.className = `${keyname}btn`;
+        
+        newBtn.textContent = `Finish ${keyname}`;
+        
 
-             }
-    })
+        for (const key in data) {
+            newDiv.innerHTML += `
+              <div><span>${key}:</span> ${data[key]} </div>             
+              `; 
+              document.getElementById('data').appendChild(newDiv);
+              document.getElementById('data').appendChild(newBtn);
+            }  
+    
+        newBtn.addEventListener('click', function () {
+          
+          const element = document.querySelector(`.${keyname}`);
+          const btnChange = document.querySelector(`.${keyname}btn`);
+          element.classList.toggle('exfin');
+          btnChange.classList.toggle('btnFin');
+
+
+          if (newBtn.textContent.includes('Finish')) {
+          newBtn.textContent = `Reset ${keyname}`;
+
+        } else {
+          newBtn.textContent = `Finish ${keyname}`;
+          }
+
+        });
+      })
   }
+
+  // const buttonToggle = document.querySelector('.toggle');
+  // buttonToggle.addEventListener('click', toggleClass);
+
+  function finishExercise(){
+    const element = document.querySelector('.Dass');
+    element.classList.toggle('exfin');
+  }
+// function finishedExercise(){
+//   document.getElementById('Deadlift').style = 'exfin'
+// }
+
+  
 
   return (
     <>
-      <div className="card">
       <h1>Gym Tracker</h1>
+      <div className="inputs">
         <form className="form">
-          <label htmlFor="exercise">Exercise:</label>
-          <input type="text" placeholder='Enter Exercise' id="exercise" name="exercise"></input>
-          <label htmlFor="ex-type">Exercise Type:</label>
-          <select id="type" name="type">
+          <label htmlFor="Exercise">Exercise:</label>
+          <input type="text" placeholder='Enter Exercise' id="Exercise" name="Exercise"></input>
+          <label htmlFor="Type">Exercise Type:</label>
+          <select id="Type" name="Type">
             <option value="Body Weight">Body Weight</option>
             <option value="Resistance">Resistance</option>
             <option value="Kgs">Kilo Grams (Kg's)</option>
             <option value="Lbs">Pounds (Lb's)</option>
           </select>
-          <label htmlFor="amount">Amount:</label>
-          <input type="number" placeholder='Amount' id="amount" name="amount"></input>
-          <label htmlFor="reps">No. of Reps:</label>
-          <input type="number" placeholder='Reps' id="reps"  name="reps"></input>
-          <label htmlFor="sets">No. of Sets:</label>
-          <input type="number" placeholder='Sets' id="sets" name="sets"></input>
+          <label htmlFor="Amount">Amount:</label>
+          <input type="number" placeholder='Amount' id="Amount" name="Amount"></input>
+          <label htmlFor="Reps">No. of Reps:</label>
+          <input type="number" placeholder='Reps' id="Reps"  name="Reps"></input>
+          <label htmlFor="Sets">No. of Sets:</label>
+          <input type="number" placeholder='Sets' id="Sets" name="Sets"></input>
           <button id="submit-btn" type="submit" >Save Exercise</button>
         </form>
       </div>
-      <div id="data">
+      <button type="button" onClick={finishExercise}>Click Me!</button>
+      <div className="data" id="data">
       </div>
     </>
   )
 }
-
-// window.onload=function(){     /*---Runs once the app is loaded---*/
-// const form = document.querySelector('form');
-
-// form.addEventListener('submit', (e) =>  {
-//   e.preventDefault();
-  
-//   // const exlist = localStorage.getItem('form')
-//   // const data = JSON.parse(exlist);
-//   // const exArr = [];
-//   // exArr.push(data);  
-//   // console.log(exArr);
-
-//   const fd = new FormData(form);
-//   const obj = Object.fromEntries(fd);
-//   const json = JSON.stringify(obj);
-//   localStorage.setItem('form', json);
-
-//   const exlist = localStorage.getItem('form');
-//   const data = JSON.parse(exlist);
-  
-//   for (const key in data) {
-//       const markup= `            
-//       <div>
-//         <span>${key}: ${data[key]} </span>
-//       </div>
-//       `;
-//   document.getElementById('data').innerHTML += markup;
-
-// }
-// })
-// }
