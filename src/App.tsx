9ee0c6da3fd51d28d480d5a51d32195b4ed7bf23
dav15jr <dnwanderajr@outlook.com>
2024-02-20 10:45:12 +0100

    Added set counter and reset counter, Added rest timer, Needs to be completed to work properly.

--- src/App.tsx
+++ src/App.tsx
@@ -13,121 +13,105 @@ export default function App() {
     form.addEventListener('submit', (e) =>  {
         e.preventDefault();
         
-        const fd = new FormData(form);
-        const obj = Object.fromEntries(fd);
+        const fd = new FormData(form);  // data from the form
+        const obj = Object.fromEntries(fd);   //objct of the form data
 
         const keyname = obj.Exercise;   // Take the exercise name and save it to be used as the key name for local storage.
 
-        const json = JSON.stringify(obj);
+        const json = JSON.stringify(obj);    //Convert the object to a string and store it in the local storage
         localStorage.setItem(`${keyname}`, json);
 
-        const exlist = localStorage.getItem(`${keyname}`);
-        const data = JSON.parse(exlist);
-
-        console.log(data);
-
-        
+        const getSets = JSON.parse(localStorage.getItem(`${keyname}`)) //Retrieve form data from local storeage and convert it back to an object 
     
 //----------------------- Display Exercises ----------------
 
         const newDiv = document.createElement("div")
-        const newBtn = document.createElement("button")
         const setBtn = document.createElement("button")
-        newDiv.className = `${keyname}`;
+        const resetBtn = document.createElement("button")
+        const timerBtn = document.createElement("button")
+         
+        
         newDiv.id = `${keyname}`;
-        newBtn.id = `${keyname}btn`;
-        newBtn.className = `${keyname}btn`;
-        setBtn.className = `${keyname}set`;
-
-        setBtn.textContent = `Set left ${data.Sets}`
-        newBtn.textContent = `Finish ${keyname}`;
+        setBtn.id = `${keyname}set`;
+        setBtn.textContent = `Start ${keyname}`
+        resetBtn.id = `${keyname}reset`;
+        resetBtn.style.display = "none";      
+        resetBtn.textContent = `Reset ${keyname}`;
+        timerBtn.id = `${keyname}timer`;
+        timerBtn.style.display = "none";
         
-
-        for (const key in data) {
-            newDiv.innerHTML += `
-              <div><span>${key}:</span> ${data[key]} </div>             
-              `; 
-              document.getElementById('data').appendChild(newDiv);
-              document.getElementById('data').appendChild(newBtn);
-              document.getElementById('data').appendChild(setBtn);
-
-            }  
-    
-        newBtn.addEventListener('click', function () {
+        for (const key in getSets) {
+          newDiv.innerHTML += `
+          <div><span>${key}:</span> ${getSets[key]} </div>             
+          `; 
+          document.getElementById('data').appendChild(newDiv);
+          }  
+          document.getElementById('data').appendChild(setBtn);
+          document.getElementById('data').appendChild(resetBtn);
+          document.getElementById('data').appendChild(timerBtn);
           
-          const element = document.querySelector(`.${keyname}`);
-          const btnChange = document.querySelector(`.${keyname}btn`);
-          element.classList.toggle('exfin');
-          btnChange.classList.toggle('btnFin');
-
-
-          if (newBtn.textContent.includes('Finish')) {
-          newBtn.textContent = `Reset ${keyname}`;
-        } else {
-          newBtn.textContent = `Finish ${keyname}`;
-          }
-        })
-
-        
-        const Counter = () => {
-          
-          const sets = 0;
-          // let setsDone += start;
-          // setsDone ++;
-          let setsLeft = data.Sets--;
-          
-          console.log(setsLeft);
-
-          const setChange = document.querySelector(`.${keyname}set`)
-          // setChange.textContent = `Sets done ${setsLeft}`
-
-          if (sets === setsLeft) {
-
-            setChange.textContent = `You Are Done`
-            setsLeft = data.Sets++
-          } else {
-
-            setChange.textContent = `Sets done ${setsLeft}`
-
-          }
-
+    const  showResetButton = () =>{  
+          resetBtn.addEventListener('click', resetExercise);
+          resetBtn.style.display = "block";
         }
 
-        setBtn.addEventListener('click', Counter);
 
-        // setBtn.addEventListener('click', function () {
-          
-        //     Counter();
-        //   const setChange = document.querySelector(`.${keyname}set`)
+    const toggleBtn = () =>{
+          element.classList.toggle('exfin');
+          setChange.classList.toggle('btnFin');
+        }
 
-        //   console.log(sets);
+    const resetExercise = () => {
+          resetBtn.style.display = "none"
+          setChange.textContent = `${count--} sets left.`
+          setBtn.disabled = false;
+          toggleBtn();
 
-        //   setSets(sets - 1)
+        }
+        
+        const setChange = document.querySelector(`#${keyname}set`);
+        const element = document.querySelector(`#${keyname}`);
+        let count = getSets.Sets;
+        
+        const countSets = () => {
+          if (count < 1) {
+            setChange.textContent = `You Are Done 🙌`
+            count = getSets.Sets;
+            setBtn.disabled = true;
+            timerBtn.style.display = "none";
+            showResetButton();
+            toggleBtn();
+            
+          } else {
+            setChange.textContent = `${count--} sets left.`
+            timerBtn.textContent = `Start Rest Timer.`
+            timerBtn.style.display = "block";
+            timerBtn.addEventListener('click', addTimer);
 
-        //   setChange.textContent = `Sets left ${sets}`
-        // })
+          }
+        }
+        setBtn.addEventListener('click', countSets);
 
-      }) 
+      const addTimer = () => {
+          let restTime = 10;
 
+          const timer = setInterval(function() {
+
+            restTime--;
+            console.log(restTime);
+            timerBtn.textContent = `${restTime}'s rest left.`
+
+              if (restTime === 0) {
+                clearInterval(timer);
+                console.log("Time's up!");
+                timerBtn.textContent = `Get back to work. 💪`
+              }
+            
+            }, 1000);
+      }
+
+  }) 
 }
-  // function finishExercise(){
-  //   const element = document.querySelector('.Dass');
-  //   element.classList.toggle('exfin');
-  // }
-
-  // const Counter = () => {
-    
-  //   setSets(sets - 1)
-
-  //   console.log(sets);
-
-  //   const setChange = document.querySelector(`.${keyname}set`)
-  //   setChange.textContent = `Sets left ${sets}`
-
-  //   const element = document.querySelector('.Dass');
-  //   element.textContent = `Sets left ${sets}`
-
-  // }
 
 
   return (
@@ -136,7 +120,7 @@ export default function App() {
       <div className="inputs">
         <form className="form">
           <label htmlFor="Exercise">Exercise:</label>
-          <input type="text" placeholder='Enter Exercise' id="Exercise" name="Exercise"></input>
+          <input type="text" placeholder='Enter Exercise' id="Exercise" name="Exercise" required></input>
           <label htmlFor="Type">Exercise Type:</label>
           <select id="Type" name="Type">
             <option value="Body Weight">Body Weight</option>
@@ -149,14 +133,14 @@ export default function App() {
           <label htmlFor="Reps">No. of Reps:</label>
           <input type="number" placeholder='Reps' id="Reps"  name="Reps"></input>
           <label htmlFor="Sets">No. of Sets:</label>
-          <input type="number" placeholder='Sets' id="Sets" name="Sets"></input>
+          <input type="number" placeholder='Sets' id="Sets" name="Sets" required></input>
           <button id="submit-btn" type="submit" >Save Exercise</button>
         </form>
       </div>
-      {/* <button type="button" onClick={Counter}>Click Me!</button> */}
       <div className="data" id="data">
       </div>
     </>
   )
 }
+
 
