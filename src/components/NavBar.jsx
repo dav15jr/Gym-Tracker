5b71aabadadc8fee8f21cabab232e9b0091f88ec
dav15jr <dnwanderajr@outlook.com>
2024-06-26 14:30:38 +0200

    Added footer.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,14 +19,14 @@ export default function NavBar({setIsLoggedIn, setWorkoutPlan, setShowForm, user
     }
 return (  
 <>
-  <nav className="navbar navbar-expand-md mb-5 px-1 px-md-5" style={{backgroundColor: "#D300FF"}}>
-  <div className="container-fluid">
+<nav className="navbar navbar-expand-lg mb-5" style={{backgroundColor: "#D300FF"}}>
+  <div className=" container-fluid container">
     <a className="navbar-brand px-sm-5" href="#">GYM APP</a>
     <button className="navbar-toggler mx-sm-5" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse justify-content-end px-md-5" id="navbarScroll">
-      <ul className="navbar-nav">
+      <ul className="navbar-nav  ">
         <li className="nav-item">
           <a className="nav-link " aria-current="page" href="#">Home</a>
         </li>
