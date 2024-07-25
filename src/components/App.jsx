ef4cf9764b6f9dc58147b6073fd1242aebaca65a
dav15jr <dnwanderajr@outlook.com>
2024-07-25 19:37:49 +0200

    Began creating custom bootstrap theme and styling colours using Sass

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,7 @@ import ProgressPage from '../pages/ProgressPage';
 import ProtectedRoute from './ProtectedRoute';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../main.css';
 
 
 function App() {
